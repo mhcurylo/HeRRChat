4b6b66232206c054ded12832f5e39364774c7652
mhcurylo <mhcurylo@gmail.com>
2015-12-09 21:36:11 +0100

    chat, version beta. skelet is ready.

--- Client/src/components/sources/SingleSource.jsx
+++ Client/src/components/sources/SingleSource.jsx
@@ -5,8 +5,8 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 export default React.createClass({
     mixins: [PureRenderMixin],
     render: function() {
-        return  <li className="source">{this.props.source.toJS()}
-                 {this.props.source.name ? this.props.source.name : 'anon'} 
+        return  <li className="source">
+                 {this.props.source.get('name')}
             </li>;
      
     }
--- Client/src/reducers/functions/sourcesHolder.js
+++ Client/src/reducers/functions/sourcesHolder.js
@@ -1,3 +1,4 @@
 export function receiveSources (sources) {
+    console.log('soruces', sources.toJS())
     return sources;
 }
